830317fbae2d00d439a517d0955429b5c311964b
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-22 10:14:34 +0700

    Работа с изображением по ссылке

--- age-check-api/main.py
+++ age-check-api/main.py
@@ -1,5 +1,6 @@
 import flask
-from flask import Flask, jsonify, request
+import re
+from flask import Flask, jsonify, request, redirect, url_for
 from flask_restful import Api, Resource, abort
 import requests
 from PIL import Image
@@ -9,12 +10,11 @@ from transformers import AutoImageProcessor, AutoModelForImageClassification
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
+@app.route('/adultcheck',methods = ['POST'])
+def login():
+    url = request.form['imageurl']
 
-    r = requests.get('{0}?raw=true'.format('https://github.com/dchen236/FairFace/blob/master/detected_faces/race_Asian_face0.jpg'))
+    r = requests.get('{0}?raw=true'.format(url))
     im = Image.open(BytesIO(r.content))
 
     processor = AutoImageProcessor.from_pretrained("nateraw/vit-age-classifier")
@@ -29,29 +29,13 @@ def create_task():
 
     # Predicted Classes
     preds = proba.argmax(1)
+    string = str(preds)
 
-    return jsonify({'task': preds}), 201
+    nums = re.findall(r'\d+', string)
+    nums = [int(i) for i in nums]
+    adult = nums[0] > 2
+
+    return str(adult)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# Get example image from official fairface repo + read it in as an image
-# r = requests.get('{0}?raw=true'.format(url))
-# im = Image.open(BytesIO(r.content))
-
-# Init model, transforms
-# processor = AutoImageProcessor.from_pretrained("nateraw/vit-age-classifier")
-# model = AutoModelForImageClassification.from_pretrained("nateraw/vit-age-classifier")
-#
-# # Transform our image and pass it through the model
-# inputs = processor(im, return_tensors='pt')
-# output = model(**inputs)
-#
-# # Predicted Class probabilities
-# proba = output.logits.softmax(1)
-#
-# # Predicted Classes
-# preds = proba.argmax(1)
-# print(str(preds))
-# return str(preds)

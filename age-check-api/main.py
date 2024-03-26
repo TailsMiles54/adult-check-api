3319c736a790a9c6e89c89284bc68fbecae6bb80
Igor Brusko <tailsmiles2015b@gmail.com>
2024-03-26 11:52:34 +0700

    Logging

--- age-check-api/main.py
+++ age-check-api/main.py
@@ -4,15 +4,14 @@ from flask import Flask, request
 from requests import get
 from PIL import Image
 from io import BytesIO
+import logging
 
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 
 ASSETS_DIR = os.path.join(os.path.dirname(__file__))
 app = Flask(__name__)
 
-@app.route('/test', methods = ['GEt'])
-def test():
-    return "Hello World!"
+logging.basicConfig(filename="age-check.log", level=logging.INFO)
 
 @app.route('/adultcheck',methods = ['POST'])
 def login():
@@ -21,11 +20,13 @@ def login():
         url = request.form['imageurl']
         r = get('{0}?raw=true'.format(url))
         im = Image.open(BytesIO(r.content))
+        logging.info('{0} url: {1}'.format(request, url))
         print('no files')
     else:
         print('has file')
         file = request.files['file']
         im = Image.open(file)
+        logging.info('{0} filename: {1}'.format(request, file.filename))
 
     im = im.convert('RGB')
 
